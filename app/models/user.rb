4439201c368e5aabe502ab1dbce3ac961d1d0fdc
Amanda Neumann <amandaneumann@me.com>
2013-11-12 20:35:12 +1100

    Remove password from edit user form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
